3aea9510c28df34b7c22d0cff52f6abb815d63ae
matthh9797 <matthewh@netextremity.com>
2025-05-02 16:12:21 +0200

    updated cloud build trigger

--- devops/terraform/cloud_build.tf
+++ devops/terraform/cloud_build.tf
@@ -1,18 +1,20 @@
 resource "google_cloudbuild_trigger" "ci" {
-  name               = "dbt-ci-trigger"
+  name               = "dbt-ci-trigger-tf"
   description        = "Trigger for dbt CI pipeline"
   disabled           = false
-  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+#   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  service_account = google_service_account.default.email
   github {
     owner = var.github_owner
     name  = var.github_repo
     push {
-      branch = "^refs/heads/feature/terraform-module$"
+      branch = "^feature/terraform-module$"
+      invert_regex = false
     }
   }
   substitutions = {
-    _REGION                = var.region
-    _ARTIFACT_REGISTRY_URL = "${var.region}-docker.pkg.dev/${var.project}/${google_artifact_registry_repository.default.name}"
+    _REGION                = "europe-west3"
+    _ARTIFACT_REGISTRY_URL = google_artifact_registry_repository.default.id
   }
   filename = "ci.cloudbuild.yaml"
 }
