f38266ed7647475d8829b1c252bd152e4de36223
matthh9797 <matthewh@netextremity.com>
2025-05-02 17:19:19 +0200

    moved cloud build

--- devops/terraform/cloud_build.tf
+++ devops/terraform/cloud_build.tf
@@ -1,20 +1,20 @@
 resource "google_cloudbuild_trigger" "ci" {
-  name               = "dbt-ci-trigger-tf"
-  description        = "Trigger for dbt CI pipeline"
-  disabled           = false
-#   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+  name        = "dbt-ci-trigger"
+  description = "Trigger for dbt CI pipeline"
+  disabled    = false
+  #   include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
   service_account = google_service_account.default.email
+  location = var.region
   github {
     owner = var.github_owner
     name  = var.github_repo
     push {
-      branch = "^feature/terraform-module$"
+      branch       = "^feature/terraform-module$"
       invert_regex = false
     }
   }
   substitutions = {
-    _REGION                = "europe-west3"
-    _ARTIFACT_REGISTRY_URL = google_artifact_registry_repository.default.id
+    _ARTIFACT_REGISTRY_URL = "${var.region}-docker.pkg.dev/${var.project}/${google_artifact_registry_repository.default.name}"
   }
-  filename = "ci.cloudbuild.yaml"
+  filename = "cloud_build/ci.cloudbuild.yaml"
 }
